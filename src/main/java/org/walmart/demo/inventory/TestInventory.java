1d7c9fa88112125f84c25eac316528be95dd8b75
Pragya Parul <parulpragya767@gmail.com>
2016-11-07 12:26:12 +0530

    first commit

---
+++ src/main/java/org/walmart/demo/inventory/TestInventory.java
@@ -0,0 +1,10 @@
+package org.walmart.demo.inventory;
+
+/**
+ * Created by p0p006o on 07/11/16.
+ */
+public class TestInventory {
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+    }
+}
